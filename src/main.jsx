2d0f62b70c525f90b00eb265974cea615a07cda3
ChipButt <jameschipbutt@hotmail.com>
2026-09-11 20:58:05 +0100

    Load QuizTaker v6 loading screen refinements

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import "./planuf-soft-ui.css";
 import "./app-wide-theme.css";
 import "./quiz-taker-v4.css";
 import "./quiz-taker-v5.css";
+import "./quiz-taker-v6.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
