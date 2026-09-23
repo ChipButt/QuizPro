82792431cba186b4c98982423498429f214c400b
ChipButt <jameschipbutt@hotmail.com>
2026-09-23 06:08:01 +0100

    Use Push Answer in staged live flow

--- src/components/SimpleLiveQuiz.jsx
+++ src/components/SimpleLiveQuiz.jsx
@@ -790,7 +790,7 @@ export default function SimpleLiveQuiz({ state, updateState, network }) {
                   disabled={reviewQuestionRevealed && !canReviewNext}
                   onClick={() => runAnswerFlow(reviewQuestionAsked, reviewQuestionRevealed)}
                 >
-                  {reviewQuestionRevealed ? <>Next <ArrowRight size={16} /></> : reviewQuestionAsked ? <><Eye size={16} /> Reveal Answer</> : <><Send size={16} /> Push Question</>}
+                  {reviewQuestionRevealed ? <>Next <ArrowRight size={16} /></> : reviewQuestionAsked ? <><Eye size={16} /> Push Answer</> : <><Send size={16} /> Push Question</>}
                 </button>
               )}
             </div>
